e77e1b2fb399324bf1e58fac04922cd26718f332
Minh Luong Van <luongvanminh@huflit.edu.vn>
2020-03-30 09:34:36 +0700

    coomit Buoi1 Buoi2

---
+++ Buoi2/VDDieuKhien/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VDDieuKhien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x=5;
+            var y=6;
+            
+            var z=9;
+            var r =x<y?y<z?(x,y,z):x<z?(x,z,y):(z,x,y):x<z?(y,x,z):y<z?(y,z,x):(z,y,x);
+
+            Console.WriteLine("r: " + r);
+        }
+    }
+}
---
+++ Buoi2/VDOOP/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace VDOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SampleClass mSample = new SampleClass();
+            //Console.WriteLine(mSample.sampleField);
+
+            // DerivedClass mDerive = new DerivedClass();
+            //mDerive.sampleMethod("test");
+            // mDerive.MethodOfDerive();
+            // DerivedClass mTest = (DerivedClass) new SampleClass();
+            // mTest.sampleMethod("mTest");
+            // mTest.myMethod();
+            SampleClass mTest = new DerivedClass();
+            mTest.sampleMethod("mTest");
+
+        }
+    }
+
+    class SampleClass
+    {
+        // Constructor
+        public SampleClass()
+        {
+            // Add code here
+            // Console.WriteLine("Constructor SampleClass");
+            // sampleField = "test sample Filed";
+            // Console.WriteLine(sampleField);
+            // sampleMethod(sampleField);
+        }
+
+        // Attrubites
+        public string sampleField;
+
+        // Methods
+        public virtual void sampleMethod(string sampleParam) 
+        {
+            Console.WriteLine("sampleMethod " + sampleParam);
+            // Console.WriteLine(sampleField);
+        } 
+
+    }
+
+    class DerivedClass:SampleClass {
+        public void MethodOfDerive() {
+            Console.WriteLine("MethodOfDerive");
+        }
+
+        public override void sampleMethod(string sampleParam) 
+        {
+            Console.WriteLine("sampleMethod of  DerivedClass " + sampleParam);
+        } 
+
+        public void myMethod() {
+            Console.WriteLine("myMethod");
+
+        }
+    }
+
+    class DerivedClass2:SampleClass {
+        public void MethodOfDerive() {
+            Console.WriteLine("MethodOfDerive2");
+        }
+
+        public override void sampleMethod(string sampleParam) 
+        {
+            Console.WriteLine("sampleMethod of  DerivedClass2 " + sampleParam);
+        } 
+    }
+}

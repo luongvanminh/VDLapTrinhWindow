20cacffeda75525ad948a163253b4856e74c8755
Minh Luong Van <luongvanminh@huflit.edu.vn>
2020-04-14 11:42:37 +0700

    Code vi du thuc hanh lop phan so va cac phep toan

---
+++ ThucHanh/Buoi2/PhanSo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Buoi2
+{
+    public class PhanSo
+    {
+        int tuSo;
+        int mauSo;
+
+        public PhanSo() { 
+            tuSo = 0;
+            mauSo = 1;
+        }
+
+        public PhanSo(int soNguyen) {
+            tuSo = soNguyen;
+            mauSo = 1;
+        }
+
+        public PhanSo(int tuSoArg, int mauSoArg) {
+            tuSo = tuSoArg;
+            mauSo = mauSoArg;
+        }
+
+        // Cong hai phan so: a / b + c / d = (a*d+b*c) / (b*d)
+        public static PhanSo operator + (PhanSo a, PhanSo b) {
+            int tuSoMoi = a.tuSo * b.mauSo +  a.mauSo * b.tuSo;
+            int mauSoMoi = a.mauSo * b.mauSo;
+            return new PhanSo(tuSoMoi, mauSoMoi);
+        }
+
+        // Tru hai phan so: a / b - c / d = (a*d-b*c) / (b*d)
+        public static PhanSo operator - (PhanSo a, PhanSo b) {
+            int tuSoMoi = a.tuSo * b.mauSo -  a.mauSo * b.tuSo;
+            int mauSoMoi = a.mauSo * b.mauSo;
+            return new PhanSo(tuSoMoi, mauSoMoi);
+        }
+
+        // Nhan hai phan so: a / b * c / d = (a*c) / (b*d)
+        public static PhanSo operator * (PhanSo a, PhanSo b) {
+            int tuSoMoi = a.tuSo * b.tuSo;
+            int mauSoMoi = a.mauSo * b.mauSo;
+            return new PhanSo(tuSoMoi, mauSoMoi);
+        }
+
+        // Chia hai phan so: a / b * c / d = (a*d) / (b*c)
+        public static PhanSo operator / (PhanSo a, PhanSo b) {
+            int tuSoMoi = a.tuSo * b.mauSo;
+            int mauSoMoi = a.mauSo * b.tuSo;
+            return new PhanSo(tuSoMoi, mauSoMoi);
+        }
+
+        // So sanh lon hon a / b va c / d => a / b - c / d > 0 <=> (a*d - b*c)/(b*d) > 0
+        public static bool operator > (PhanSo a, PhanSo b) {
+            PhanSo kq = a - b;
+            return kq.tuSo > 0;
+        }
+
+        // So sanh nho hon a / b va c / d => a / b - c / d < 0
+        public static bool operator < (PhanSo a, PhanSo b) {
+            PhanSo kq = a - b;
+            return kq.tuSo < 0;
+        }
+
+        // So sanh lon hon hoac bang a / b va c / d => a / b - c / d >= 0 <=> (a*d - b*c)/(b*d) >= 0
+        public static bool operator >= (PhanSo a, PhanSo b) {
+            return !(a<b);
+        }
+
+        // So sanh lon hon hoac bang a / b va c / d => a / b - c / d >= 0 <=> (a*d - b*c)/(b*d) >= 0
+        public static bool operator <= (PhanSo a, PhanSo b) {
+            return !(a>b);
+        }
+
+        // So sanh bang a / b va c / d => a / b - c / d = 0
+        public static bool operator == (PhanSo a, PhanSo b) {
+            PhanSo kq = a - b;
+            return kq.tuSo == 0;
+        }
+
+        // So sanh khac a / b va c / d => a / b - c / d != 0
+        public static bool operator != (PhanSo a, PhanSo b) {
+            return !(a==b);
+        }
+
+        public void prettyPrint() {
+            Console.WriteLine("Gia tri la: " + tuSo + " / " + mauSo);
+        }
+    }
+}
---
+++ ThucHanh/Buoi2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Buoi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nhap gia tri phan so a tu nguoi dung
+            Console.WriteLine("Nhap tu so cua a:");
+            int tuSoCuaA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap mau so cua a:");
+            int mauSoCuaA = Convert.ToInt32(Console.ReadLine());
+            PhanSo phanSoA = new PhanSo(tuSoCuaA, mauSoCuaA);
+
+            // Nhap gia tri phan so b tu nguoi dung
+            Console.WriteLine("Nhap tu so cua b:");
+            int tuSoCuaB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap mau so cua b:");
+            int mauSoCuaB = Convert.ToInt32(Console.ReadLine());
+            PhanSo phanSoB = new PhanSo(tuSoCuaB, mauSoCuaB);
+
+            PhanSo kq = phanSoA - phanSoB;
+
+            kq.prettyPrint();
+
+            if (phanSoA >= phanSoB) {
+                Console.WriteLine("Phan so A lon hon hoac bang phan so b");
+            }
+
+            if (phanSoA < phanSoB) {
+                Console.WriteLine("Phan so A nho hon phan so b");
+            }
+        }
+    }
+}

eb3f58a6284b23f431e88fcd68365ca5e43cf510
Minh Luong Van <luongvanminh@huflit.edu.vn>
2020-04-20 10:09:41 +0700

    Vd bai 1 buoi thuc hanh 3

---
+++ ThucHanh/Buoi3/Bai1/Mang1Chieu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Bai1
+{
+    public class Mang1Chieu
+    {
+        private int[] arr = new int[100];
+
+        public Mang1Chieu() {}
+
+        public Mang1Chieu(int soPhanTu) {
+            this.arr = new int [soPhanTu];
+        }
+
+        public int length {
+            get { return arr.Length; }
+        }
+
+        // operator []
+        public int this[int i] {
+            get { return arr[i]; }
+            set {
+                arr[i] = value;
+            }
+        }
+
+        public void prettyPrint() {
+            foreach (var i in arr) {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public void printEven() {
+            // foreach (var i in arr) {
+            //     if (i%2==0)
+            //         Console.Write(i + " ");
+            // }
+
+            for (int i = 0; i < this.length; i++) {
+                if (this[i]%2==0)
+                    Console.Write(this[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ThucHanh/Buoi3/Bai1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mang1Chieu mMang = new Mang1Chieu(3);
+
+            for (int i = 0; i<mMang.length; i++) {
+                Console.WriteLine("Nhap gia tri phan tu thu " + i + " :");
+                mMang[i] = int.Parse(Console.ReadLine());
+            }
+
+            mMang.prettyPrint();
+            Console.Write("Cac so chan cua day la: ");
+            mMang.printEven();
+        }
+    }
+}

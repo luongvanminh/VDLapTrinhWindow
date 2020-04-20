7964a69dff42d695fe24a6d9564213daaa6d7d59
Minh Luong Van <luongvanminh@huflit.edu.vn>
2020-04-20 16:08:10 +0700

    Giai bai 3 cua buoi 3 thuc hanh

---
+++ ThucHanh/Buoi3/Bai3/DayPhanSo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Bai3
+{
+    public class DayPhanSo
+    {
+        PhanSo[] arrPhanSo;
+        public int soLuongPhanTu;
+
+        public DayPhanSo() {
+            arrPhanSo = new PhanSo[0];
+        }
+
+        public DayPhanSo(int soLuongPhanTu) {
+            arrPhanSo = new PhanSo[soLuongPhanTu];
+            this.soLuongPhanTu = soLuongPhanTu;
+        }
+
+        // operator []
+        public PhanSo this[int i] {
+            get { return arrPhanSo[i]; }
+            set { arrPhanSo[i] = value; }
+        }
+
+        public void KhoiTaoDayPhanSo() {
+            Console.WriteLine("Vui long nhap day phan so");
+            for (int i = 0; i < this.soLuongPhanTu; i++) {
+                // PhanSo mPhanSo = this[i];
+                Console.Write("Vui long nhap tu so cua phan so thu " + i + ":");
+                int tuSoMoi = int.Parse(Console.ReadLine());
+                Console.Write("Vui long nhap mau so cua phan so thu " + i + ":");
+                int mauSoMoi = int.Parse(Console.ReadLine());
+                this[i] = new PhanSo(tuSoMoi, mauSoMoi);
+            }
+        }
+
+        public void prettyPrint() {
+            Console.Write("Day so la: ");
+            for (int i = 0; i < this.soLuongPhanTu; i++) {
+                this[i].prettyPrint();
+                if (i != this.soLuongPhanTu-1)
+                    Console.Write(", ");     
+            }
+            Console.WriteLine();
+        }
+
+        public PhanSo TongDayPhanSo() {
+            PhanSo kq = new PhanSo();
+            for (int i = 0; i < this.soLuongPhanTu; i++) {
+                kq = kq + this[i];
+            }
+
+            return PhanSo.Reduce(kq);
+        }
+    }
+}
---
+++ ThucHanh/Buoi3/Bai3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vui long nhap so luong phan tu cua day phan so: ");
+            int n = int.Parse(Console.ReadLine());
+            DayPhanSo mDayPhanSo = new DayPhanSo(n);
+            mDayPhanSo.KhoiTaoDayPhanSo();
+            mDayPhanSo.prettyPrint();
+            Console.Write("Tong cua day so nay la: " );
+            mDayPhanSo.TongDayPhanSo().prettyPrint();
+            Console.WriteLine();
+        }
+    }
+}

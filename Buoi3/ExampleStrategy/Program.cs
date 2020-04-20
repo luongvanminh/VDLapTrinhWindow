218c971eb92e14577f8464bc2c04ebc46698f3be
Minh Luong Van <luongvanminh@huflit.edu.vn>
2020-04-20 07:38:26 +0700

    Vi du ve try catch

--- Buoi3/ExampleStrategy/Program.cs
+++ Buoi3/ExampleStrategy/Program.cs
@@ -9,6 +9,25 @@ namespace ExampleStrategy
             BaseStrategy baseStrategy = new StrategySortMethod();
             MyList mList = new MyList(baseStrategy);
             mList.Display();
+
+            try {
+                // Vd ve Exception
+
+                // InvalidCastException
+                StrategyEvenMethod mStrategy = (StrategyEvenMethod) new BaseStrategy();
+                int [] arr = new int[4] { 1, 2, 3, 4};
+                mStrategy.command(arr);
+
+                // NullReferenceException:
+                StrategySortMethod strategySort = new StrategySortMethod();
+                strategySort = null;
+                strategySort.command(arr);
+
+            } catch (InvalidCastException e) {
+                Console.WriteLine("Xay ra ngoai le: " + e.ToString());
+            } catch (Exception e) {
+                Console.WriteLine("Xay ra ngoai le: " + e.ToString());
+            }
         }
     }
 }

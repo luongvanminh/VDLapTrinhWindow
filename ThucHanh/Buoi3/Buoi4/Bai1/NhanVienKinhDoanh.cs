9349e5dd950d576de10a053a4dfd9d5293ea6c7a
Minh Luong Van <luongvanminh@huflit.edu.vn>
2020-04-21 10:30:04 +0700

    Giai bai 1 buoi 4 thuc hanh

---
+++ ThucHanh/Buoi3/Buoi4/Bai1/NhanVienKinhDoanh.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bai1
+{
+    public class NhanVienKinhDoanh:NhanVien 
+    {
+        public static int TIEN_CUA_MOI_HOP_DONG = 500000;
+        private int _soLuongHopDong = 0;
+
+        public int soLuongHopDong {
+            get { return _soLuongHopDong; }
+            set { _soLuongHopDong = value; }
+        }
+
+        public int _luongCoban = 0;
+
+        public int luongCoBan {
+            get { return _luongCoban; }
+            set { _luongCoban = value; }
+        }
+
+        public NhanVienKinhDoanh(string MSNV, string Hoten):base(MSNV, Hoten) {}
+
+        public override int TinhLuong() {
+            return this.luongCoBan + this.soLuongHopDong * NhanVienKinhDoanh.TIEN_CUA_MOI_HOP_DONG;
+        }
+    }
+}
---
+++ ThucHanh/Buoi3/Buoi4/Bai1/NhanVienSanXuat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bai1
+{
+    public class NhanVienSanXuat:NhanVien 
+    {
+        public static int TIEN_CONG_MOI_SAN_PHAM = 1000;
+        private int _soLuongSanPham = 0;
+
+        public int soLuongSanPham {
+            get { return _soLuongSanPham; }
+            set { _soLuongSanPham = value; }
+        }
+
+        public NhanVienSanXuat(string MSNV, string Hoten):base(MSNV, Hoten) {}
+
+        public override int TinhLuong() {
+            // if (this.soLuongSanPham < 3000) {
+            //     return this.soLuongSanPham * NhanVienSanXuat.TIEN_CONG_MOI_SAN_PHAM;
+            // } else {
+            //     return (int) ((this.soLuongSanPham * NhanVienSanXuat.TIEN_CONG_MOI_SAN_PHAM) * 1.05);
+            // } 
+            int kq = this.soLuongSanPham * NhanVienSanXuat.TIEN_CONG_MOI_SAN_PHAM;
+            if (this.soLuongSanPham >= 3000)
+                kq = (int) (kq*1.05);
+            return kq;
+        }
+    }
+}

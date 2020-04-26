7323905511d493d8cf9384ae29fed30171c1e2f3
Minh Luong Van <luongvanminh@huflit.edu.vn>
2020-04-26 15:29:41 +0700

    commit all

---
+++ Buoi3/ExampleTryCatch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExampleTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                Console.WriteLine("Nhap vao bien a: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Nhap vao bien b: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                double c = a / b;
+                Console.WriteLine("Gia tri cua a / b la: " + c);
+            } catch (Exception e) {
+                Console.WriteLine("Co xay ra ngoai le la: " + e.ToString());
+                
+            }
+
+
+        }
+    }
+}
